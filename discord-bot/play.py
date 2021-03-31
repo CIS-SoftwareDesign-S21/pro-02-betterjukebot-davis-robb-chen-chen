a51c5020980f525a18ad8eadf9cb92c1be8713f1
Christian Davis <Phaceial@gmail.com>
2021-03-31 00:03:24 -0400

    Revert "Merge pull request #13 from CIS-SoftwareDesign-S21/seekbar"
    This reverts commit 7af0c7a287da52f8ffc017efd95cab22410a9b4d, reversing
    changes made to
    a606680ac5894f8020fa44f32727d46005d5634b.

--- discord-bot/play.py
+++ discord-bot/play.py
@@ -2,50 +2,17 @@ import discord
 from discord.ext import commands
 import os
 import youtube_dl
-import urllib.parse
 
 # Credentials
 TOKEN = "ODIzNDI0MjQ3ODYyMDAxNjY0.YFgnlw.VZk0hl3tb9iYhNb1I0AdVgvcUqI"
 
 # Creating the Bot
-bot = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix="!")
 
-
-@bot.event
-async def on_ready():
-    print("Connected to bot: {}".format(bot.user.name))
-    print("Bot ID: {}".format(bot.user.id))
-
-
-# Command
-
-
-@bot.command()
-async def hello_world(ctx):
-    await ctx.send("Hello World!")
-
-
-@bot.command()
-async def soup(ctx):
-    await ctx.send("Soup!")
-
-
-@bot.command()
-async def goober(ctx):
-    await ctx.send("Becca")
-
-
-@bot.command()
+@client.command()
 async def youtube(ctx, *, search):
-    await ctx.send("Doing youtube searches soon")
-
-
-@bot.command()
-async def seek(ctx, timestamp: int):
-    await ctx.send("Doing seeking soon")
-
-
-@bot.command()
+    
+@client.command()
 async def play(ctx, url: str):
     song = os.path.isfile("song.mp3")
     try:
@@ -59,7 +26,7 @@ async def play(ctx, url: str):
 
     voiceChannel = discord.utils.get(ctx.guild.voice_channels, name="General")
     await voiceChannel.connect()
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
 
     ydl_opts = {
         "format": "bestaudio/best",
@@ -80,33 +47,33 @@ async def play(ctx, url: str):
     voice.play(discord.FFmpegPCMAudio("song.mp3"))
 
 
-@bot.command()
+@client.command()
 async def stop(ctx):
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     voice.stop()
 
 
-@bot.command()
+@client.command()
 async def leave(ctx):
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     if voice.is_connected():
         await voice.disconnect()
     else:
         await ctx.send("The bot is not in a channel.")
 
 
-@bot.command()
+@client.command()
 async def pause(ctx):
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     if voice.is_playing():
         voice.pause()
     else:
         await ctx.send("Nothing is playing.")
 
 
-@bot.command()
+@client.command()
 async def resume(ctx):
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     if voice.is_paused():
         voice.resume()
     else:
@@ -114,4 +81,4 @@ async def resume(ctx):
 
 
 # Running the bot
-bot.run(TOKEN)
+client.run(TOKEN)
