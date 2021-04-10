e970a311dac4c2338338dcb99c135aa4df247dd1
Darren Chen <tuj72544@temple.edu>
2021-04-10 18:46:03 -0400

    Changed BeautifulSoup method call in queue command

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -347,8 +347,8 @@ async def queue(ctx):
     global song_queue
     for song in song_queue:
         index = song_queue.index(song) + 1
-        song_title = BeautifulSoup(urllib.request.urlopen(song))
-        await ctx.send(f"#{index}: {song_title}")
+        soup = BeautifulSoup(urllib.request.urlopen(song), "html.parser")
+        await ctx.send(f"#{index}: {soup.title}")
 
 
 # # was working, then stopped. May need a new library or implement manual solution
