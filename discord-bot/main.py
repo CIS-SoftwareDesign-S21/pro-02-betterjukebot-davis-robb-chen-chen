861694971ae4d3aef8e6d17562077f988574b56d
ShigureChen <tuk93581@temple.edu>
2021-04-11 10:16:23 +0800

    Fix bugs

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -272,18 +272,13 @@ async def setidle(ctx, seconds: int):
 async def lyrics(ctx):
     global currentSong
 
-    song_detail = musixmatch.matcher_track_get(currentSong, currentSong)
-    pprint(song_detail)
-    song_title = song_detail["title"]
-    song_artist = song_detail["artist"]
-
-    lyrics_display = musixmatch.matcher_lyrics_get(song_title, song_artist)
+    lyrics_display = musixmatch.matcher_lyrics_get(currentSong, currentSong)
 
     if lyrics_display is not None:
         pprint(lyrics_display)
         lyrics_to_send = lyrics_display["message"]["body"]["lyrics"]["lyrics_body"]
         await ctx.send(
-            f"```Now playing: {song_title}\nArtist: {song_artist} \n\n\n{lyrics_to_send}```"
+            f"```Now playing: {currentSong}\nArtist: {currentSong} \n\n\n{lyrics_to_send}```"
         )
     else:
         await ctx.send("Cannot find lyrics for this song :(")
