42683963015f3ec33272327146d03a405b43210c
Christian Davis <Phaceial@gmail.com>
2021-04-13 10:07:48 -0400

    Add missing FIles

---
+++ discord-bot/main2.py
@@ -0,0 +1,48 @@
+import discord
+import os
+from discord.ext import commands
+from secrets import DISCORD_TOKEN
+
+bot = commands.Bot(command_prefix="!")
+
+
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f"cogs.{extension}")
+
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f"cogs.{extension}")
+
+
+@bot.command()
+async def hello_world(ctx):
+    await ctx.send("Hello World!")
+
+
+@bot.command()
+async def soup(ctx):
+    await ctx.send("Soup!")
+
+
+@bot.command()
+async def goober(ctx):
+    await ctx.send("Becca")
+
+
+@bot.command()
+async def angry(ctx):
+    await ctx.send(":angry:")
+
+
+@bot.command()
+async def angryarray(ctx):
+    await ctx.send(":angry::rage::angry:\n:rage::angry::rage:" "\n:angry::rage::angry:")
+
+
+for filename in os.listdir("cogs"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"cogs.{filename[:-3]}")
+
+bot.run(DISCORD_TOKEN)
