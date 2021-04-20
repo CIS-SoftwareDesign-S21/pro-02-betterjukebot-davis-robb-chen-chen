a25bc9b5102c0445f3b63f238c6279206436318f
Christian Davis <Phaceial@gmail.com>
2021-04-20 11:41:54 -0400

    needs tobe discord-bot/cogs for heroku

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -23,7 +23,7 @@ async def unload(ctx, extension):
     bot.unload_extension(f"cogs.{extension}")
 
 
-for filename in os.listdir("cogs"):
+for filename in os.listdir("discord-bot/cogs"):
     if filename.endswith(".py"):
         bot.load_extension(f"cogs.{filename[:-3]}")
 
